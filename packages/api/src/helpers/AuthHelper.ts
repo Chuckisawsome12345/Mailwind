3756233e410228a1440f1b129670357a6578e2b1
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-06-08 15:19:45 -0700

    chore(API): make fetchGitHubUser helper function return type GitHubUser

--- packages/api/src/helpers/AuthHelper.ts
+++ packages/api/src/helpers/AuthHelper.ts
@@ -1,8 +1,9 @@
+import { GitHubUser } from "../models/GitHubUser";
 import querystring from "querystring";
 import axios from "axios";
 
 export class AuthHelper {
-  public async fetchGitHubUser(code: string) {
+  public async fetchGitHubUser(code: string): Promise<GitHubUser> {
     const githubAccessToken = await axios
       .post(
         `https://github.com/login/oauth/access_token?client_id=${process.env.MAILWIND_GITHUB_CLIENT_ID}&client_secret=${process.env.MAILWIND_GITHUB_CLIENT_SECRET}&code=${code}`
